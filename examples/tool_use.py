1aba838b68b520aa6e437ddf12fb94e716d917de
Baagsma <barthaagsma@outlook.com>
2024-12-07 22:24:06 +0100

    Updated tool_use example

--- examples/tool_use.py
+++ examples/tool_use.py
@@ -1,10 +1,25 @@
+import json
 from datetime import datetime
-from json import dumps
 from time import strftime
+from typing import Annotated
 
 import requests
+from pydantic import BaseModel, Field
 
 from PBQA import DB, LLM
+
+
+class Weather(BaseModel):
+    latitude: float
+    longitude: float
+    time: Annotated[
+        str, Field(pattern=r"^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}$")
+    ]
+
+
+class ThinkAndAnswer(BaseModel):
+    thought: str
+    answer: str
 
 
 def get_forecast(
@@ -37,40 +52,54 @@ class Agent:
 
     def ask_weather(self, input: str):
         weather_query = self.llm.ask(
-            input=input,
+            input={
+                "query": input,
+                "now": strftime("%Y-%m-%d %H:%M"),
+            },
             pattern="weather",
             model="llama",
-            external={"now": strftime("%Y-%m-%d %H:%M")},
-        )
+        )["response"]
 
-        print(f"Query:\n{dumps(weather_query, indent=4)}\n")
+        print(f"Query:\n{json.dumps(weather_query, indent=4)}\n")
 
         forecast = get_forecast(**weather_query)
         forecast = self._format_forecast(forecast)
 
-        print(f"Forecast:\n{dumps(forecast, indent=4)}\n")
+        print(f"Forecast:\n{json.dumps(forecast, indent=4)}\n")
 
         return self.llm.ask(
-            input=input,
-            pattern="answer_json",
+            input={
+                "query": input,
+                "json": json.dumps(forecast),
+            },
+            pattern="thinkandanswer",
             model="llama",
-            external={"json": dumps(forecast)},
-        )
+        )["response"]
 
     def _format_forecast(self, forecast: dict):
         return {
-            "temperature": f'{forecast["temperature"]}C',
-            "precipitation_probability": f'{forecast["precipitation_probability"]}%',
-            "precipitation": f'{forecast["precipitation"]}mm',
-            "cloud_cover": f'{forecast["cloud_cover"]}% coverage',
+            "temperature": f'{forecast["temperature"]} C',
+            "precipitation_probability": f'{forecast["precipitation_probability"]} %',
+            "precipitation": f'{forecast["precipitation"]} mm',
+            "cloud_cover": f'{forecast["cloud_cover"]} % coverage',
         }
 
 
-db = DB(path="examples/db")
-db.load_pattern("examples/weather.yaml")
-db.load_pattern("examples/answer_json.yaml")
+db = DB(host="localhost", port=6333, reset=True)
+db.load_pattern(
+    schema=Weather,
+    examples="examples/weather.yaml",
+    system_prompt="Your job is to translate the user's input into a weather query object. The object contains the latitude, longitude, and time of the weather query. Reply with the json for the weather query and nothing else.",
+    input_key="query",
+)
+db.load_pattern(
+    schema=ThinkAndAnswer,
+    examples="examples/answer_json.yaml",
+    system_prompt="Use the provided JSON object to answer the query. Leave out any irrelevant information. If the data looks insufficient at face value, try to think step by step to come to a helpful answer. Reply with an answer to the query without mentioning the JSON object or any of its properties. Unless asked, your final answer should not contain any specific numbers or values from the JSON object.",
+    input_key="query",
+)
 
-llm = LLM(db=db, host="localhost")
+llm = LLM(db=db, host="192.168.0.91")
 llm.connect_model(
     model="llama",
     port=8080,
@@ -81,4 +110,4 @@ llm.connect_model(
 agent = Agent(db=db, llm=llm)
 
 response = agent.ask_weather("Could I see the stars tonight?")
-print(f"Response:\n{dumps(response, indent=4)}")
+print(f"Response:\n{json.dumps(response, indent=4)}")
